79bcde063e0f22398de260df4c7d7b9c590bf6a1
andrewvu-cs <andrewvu.cs@gmail.com>
2019-02-15 10:48:02 -0800

    Added more functionality

--- manga_bot.py
+++ manga_bot.py
@@ -13,65 +13,76 @@ from selenium.common.exceptions import TimeoutException
 import re
 import time
 
-# class MangaBot(object):
-#     def __init__(self, items):
-        # self.manga_url = "https://www.mangadex.org"
-        # self.items = items
-        # self.
-def search_items(item):
-    manga_url = "https://www.mangadex.org"
-    chromedriver = "/Users/andrewvu/Documents/chromedriver"
-    myOptions = Options()
-    driver = webdriver.Chrome(chromedriver, options = myOptions)
-
-    driver.get(manga_url)
+#I use mangadex.org as my choice of reading site
+class mangabot(object):
+    def __init__(self, mangas):
+        self.manga_url = "https://www.mangadex.org"
+        self.mangas = mangas
+        #Srapes on google chrome 
+        self.chromedriver = "/Users/andrewvu/Documents/chromedriver"
+        self.myOptions = Options()
+        self.driver = webdriver.Chrome(self.chromedriver, options = self.myOptions)
+        self.driver.get(self.manga_url)
     
-    change_settings(driver)
 
-    search_input = driver.find_element_by_id("quick_search_input")
-    search_input.send_keys(item)
+    def search_manga(self):
+        #changes settings to find only English chapters on website
+        self.change_settings(self.driver)
 
-    time.sleep(3)
+        directory_urls =[]
+        latest_chapters = []
+        chapter_urls = []
 
-    search_button = driver.find_element_by_id("quick_search_button")
-    search_button.click()
+        for manga in self.mangas:
+            search_input = self.driver.find_element_by_id("quick_search_input")
+            search_input.send_keys(manga)
 
-    time.sleep(3)
+            time.sleep(3)
 
-    manga_page = driver.find_element_by_link_text(item)
-    manga_page.click()
+            search_button = self.driver.find_element_by_id("quick_search_button")
+            search_button.click()
 
-    time.sleep(3)
+            time.sleep(3)
 
-    manga_chapter = driver.find_element_by_xpath('//*[@id="content"]/div[3]/div/div[2]/div/div/div[2]/a')
-    manga_chapter.click()
-    time.sleep(3)
+            #grabs directory url of specified manga
+            manga_page = self.driver.find_element_by_link_text(manga)
+            manga_page.click()
+            directory_urls.append(self.driver.current_url)
 
-    chapter = driver.find_element_by_id("jump-chapter").text
-    print(chapter[0:6])
-    latest_chapter = chapter[0:6]
+            time.sleep(3)
 
-    url = driver.current_url
-    print(url)
+            manga_chapter = self.driver.find_element_by_xpath('//*[@id="content"]/div[3]/div/div[2]/div/div/div[2]/a')
+            manga_chapter.click()
+            time.sleep(3)
 
-def change_settings(driver):
-    eng_settings = driver.find_element_by_xpath('//*[@id="homepage_cog"]/a/span')
-    eng_settings.click()
+            chapter = self.driver.find_element_by_id("jump-chapter").text
+            print(chapter[0:15])
+            latest_chapters.append(chapter[0:15])
+            chapter_urls.append(self.driver.current_url)
 
-    time.sleep(3)
+        return directory_urls, latest_chapters, chapter_urls
 
-    eng_settings_dropdown = driver.find_element_by_xpath('//*[@id="homepage_settings_form"]/div[2]/div/div/button/div/div/div')
-    eng_settings_dropdown.click()
+    #Changes the settings to english
+    def change_settings(self, driver):
+        eng_settings = self.driver.find_element_by_xpath('//*[@id="homepage_cog"]/a/span')
+        eng_settings.click()
 
-    time.sleep(3)
+        time.sleep(3)
 
-    eng_lang = driver.find_element_by_xpath('//*[@id="homepage_settings_form"]/div[2]/div/div/div/div[2]/ul/li[11]/a')
-    eng_lang.click()
-    time.sleep(3)
+        eng_settings_dropdown = self.driver.find_element_by_xpath('//*[@id="homepage_settings_form"]/div[2]/div/div/button/div/div/div')
+        eng_settings_dropdown.click()
 
-    save_button = driver.find_element_by_xpath('//*[@id="homepage_settings_button"]')
-    save_button.click()
-    time.sleep(3)
+        time.sleep(3)
+
+        eng_lang = self.driver.find_element_by_xpath('//*[@id="homepage_settings_form"]/div[2]/div/div/div/div[2]/ul/li[11]/a')
+        eng_lang.click()
+        time.sleep(3)
+
+        save_button = self.driver.find_element_by_xpath('//*[@id="homepage_settings_button"]')
+        save_button.click()
+        time.sleep(3)
+
+       
 
 
-search_items("Solo Leveling")
+# search_items("Solo Leveling")
--- update_mangas.py
+++ update_mangas.py
@@ -1,21 +1,37 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-
+from manga_bot import mangabot
 
 class MangaUpdater(object):
     def __init__(self, spreadsheet_name):
-        scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+        self.manga_col = 1
+        self.directory_url = 2
+        self.chapter = 3
+        self.chapter_url = 4
 
-        creds = ServiceAccountCredentials.from_json_keyfile_name('client.json', scope)
+        scope = ['https://spreadsheets.google.com/feeds',
+        'https://www.googleapis.com/auth/drive']
+
+        #Include your JSON file
+        creds = ServiceAccountCredentials.from_json_keyfile_name('.....json', scope)
 
         client = gspread.authorize(creds)
 
         self.sheet = client.open(spreadsheet_name).sheet1
 
-manga_updater = MangaUpdater("manga_chapters")
+    def process_manga_list(self):
+        mangas = self.sheet.col_values(self.manga_col)[1:]
 
+        manga_bot = mangabot(mangas)
+        directory_urls, chapter, chapter_urls = manga_bot.search_manga()
 
+        print("Updating spreadsheet.")
+        for i in range(len(chapter)):
+            self.sheet.update_cell(i+2, self.directory_url, directory_urls[i])
+            self.sheet.update_cell(i+2, self.chapter, chapter[i])
+            self.sheet.update_cell(i+2, self.chapter_url, chapter_urls[i])
+    
 
-#         stuff = sheet.get_all_records()
+manga_updater = MangaUpdater("...") #INCLUDE YOUR SPREADSHEET NAME MADE ON GOOGLE DRIVE
+manga_updater.process_manga_list()
 
-# print(stuff)
